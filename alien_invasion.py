5b8f211ca552a5c9e14df309e3af39c651459c26
night-sky-koi <171149510@qq.com>
2022-08-02 15:23:40 +0800

    :fire:

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
+        #设置背景色
+        self.bg_color = (230,230,230)
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
@@ -21,6 +24,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            #每次循环都重新绘制屏幕
+            self.screen.fill(self.bg_color)
 
             #让最近绘制的屏幕可见。
             pygame.display.flip()
